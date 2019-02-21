eb03c6822eb7bd54f5b17671940789b3f520f284
Theresia Coanata <theresiacoa@DESKTOP-K5BVEFS.localdomain>
2019-02-21 20:25:17 +0700

    play room done

---
+++ src/components/formNewRoom.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <b-button v-b-modal.modalPrevent>Create Room</b-button>
+
+    <!-- Modal Component -->
+    <b-modal
+      id="modalPrevent"
+      ref="modal"
+      title="Submit Room Name"
+      @ok="handleOk"
+      @shown="clearName"
+    >
+      <form @submit.stop.prevent="createRoom">
+        <b-form-input type="text" placeholder="Enter room name" v-model="roomName"/>
+      </form>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+import db from '@/script/config.js'
+import swal from 'sweetalert'
+import { mapState } from 'vuex'
+
+export default {
+  name: 'newRoom',
+  data () {
+    return {
+      roomName: '',
+      statusRoom: false,
+      player1: localStorage.getItem('username'),
+      point1: 0,
+      player2: null,
+      point2: 0
+    }
+  },
+  methods: {
+    clearName () {
+      this.roomName = ''
+    },
+    handleOk (evt) {
+      evt.preventDefault()
+      if (!this.roomName) {
+        swal('Oops!', 'Please enter room name', 'error')
+      } else {
+        this.createRoom()
+      }
+    },
+    createRoom: function (val) {
+      console.log(this, `-------- create room`)
+      this.$store.dispatch('createNewRoom', { ...this })
+
+      // clear roomName and close modal
+      this.clearName()
+      this.$nextTick(() => {
+        this.$refs.modal.hide()
+      })
+    }
+  }
+}
+</script>
--- src/store.js
+++ src/store.js
@@ -1,16 +1,50 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import db from './script/config'
+import swal from 'sweetalert'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    rooms: [],
+    roomId: '',
+    question: '',
+    player1: '',
+    player2: '',
+    point1: 0,
+    point2: 0
   },
   mutations: {
+    createNewRoom (state, payload) {
+      state.roomId = payload.id
+      state.statusRoom = payload.statusRoom
+      state.player1 = payload.player1
+    }
 
   },
   actions: {
-
+    createNewRoom ({ commit }, room) {
+      console.log(`----room`, room)
+      let res = {
+        title: room.roomName,
+        player1: localStorage.getItem('username'),
+        player2: room.player2,
+        statusRoom: room.statusRoom,
+        point1: room.point1,
+        point2: room.point2
+      }
+      db.collection('rooms').add(res)
+        .then(function (docRef) {
+          commit('createNewRoom', {
+            id: docRef.id,
+            statusRoom: res.statusRoom,
+            player1: res.player1
+          })
+        })
+        .catch(function (err) {
+          swal('Oops!', err, 'error')
+        })
+    }
   }
 })
