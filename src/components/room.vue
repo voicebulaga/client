d305b8be3bec0bd6d928011bf97048ba2b346193
Mahdihrs <mahdiharis@outlook.com>
2019-02-22 14:15:12 +0700

    game room methods applied

--- src/components/room.vue
+++ src/components/room.vue
@@ -15,25 +15,31 @@
       <br>
 
       <center>
-          <div>
+        <div>
           <div class="searchbar container" style="max-width: 30%; display:inline-block;">
-          <input
-            v-model="answer"
-            class="search_input form-control"
-            value
-            type="text"
-            name
-            placeholder="Input text.."
-            id="searchTitle"
-          >
+            <input
+              v-model="answer"
+              class="search_input form-control"
+              value
+              type="text"
+              name
+              placeholder="Input text.."
+              id="searchTitle"
+            >
           </div>
-          <button type="button" class="btn btn-secondary" style="border-radius: 40%">
+          <button
+            @click="addPointAndCheckWinner"
+            type="button"
+            class="btn btn-secondary"
+            style="border-radius: 40%"
+          >
             <i class="fas fa-check-circle"></i>
           </button>
         </div>
       </center>
       <br>
-      <center><button
+      <center>
+        <button
           @click.prevent="record"
           type="button"
           class="btn btn-secondary"
@@ -47,57 +53,88 @@
 </template>
 
 <script>
-import newRoom from '@/components/formNewRoom.vue'
-import waiting from '@/components/waiting.vue'
-import { mapState } from 'vuex'
+import newRoom from "@/components/formNewRoom.vue";
+import waiting from "@/components/waiting.vue";
+import { mapState } from "vuex";
+import alertify from 'alertifyjs'
 
 export default {
-  name: 'room',
+  name: "room",
   components: {
     waiting
   },
-  data () {
+  data() {
     return {
-      answer: ''
-    }
+      answer: ""
+    };
   },
   methods: {
-    record () {
+    record() {
       window.SpeechRecognition =
-        window.webkitSpeechRecognition || window.SpeechRecognition
-      const recognition = new SpeechRecognition()
-      recognition.start()
+        window.webkitSpeechRecognition || window.SpeechRecognition;
+      const recognition = new SpeechRecognition();
+      recognition.start();
       recognition.onresult = event => {
-        const speechToText = event.results[0][0].transcript
-        this.answer = speechToText
-      }
-      recognition.onspeechend = function () {
-        recognition.stop()
+        const speechToText = event.results[0][0].transcript;
+        this.answer = speechToText;
+      };
+      recognition.onspeechend = function() {
+        recognition.stop();
+      };
+    },
+    addPointAndCheckWinner() {
+      console.log(this.questions[this.ques].answer);
+      //kalo ques-nya udah 10, cek siapa pemenangnya abis itu delete roomnya
+      if (this.ques == 10) {
+        if (localStorage.getItem('username') === this.player1) {
+          if (this.point1 > this.point2) {
+            console.log(this.player1, " won the gayme");
+            alertify.message(`Congratulations ${localStorage.getItem('username')} you won the game`)
+          } else {
+            console.log(this.player2, " loseeee");
+            alertify.message(`Sorry ${localStorage.getItem('username')} you lose. You can try it again anytime!`)
+          }
+        } else {
+          if (this.point1 < this.point2) {
+            console.log(this.player2, " won the gayme");
+            alertify.message(`Congratulations ${localStorage.getItem('username')} you won the game`)
+          } else {
+            console.log(this.player1, " loseeee",);
+            alertify.message(`Sorry ${localStorage.getItem('username')} you lose. You can try it again anytime!`)
+          }          
+        }
+        this.$store.dispatch("deleteRoom", this.$route.params.id);
+      } else {
+        if (this.questions[this.ques].answer === this.answer) {
+          this.$store.dispatch(this.$route.params.id);
+          alertify.message('Great!')
+        } else {
+          let nextQues = this.ques + 1;
+          this.$store.commit("getQues", nextQues);
+          alertify.message('Thank you, next!')
+        }
       }
     }
   },
-  created () {
-    this.$store.dispatch('getQuestions')
-    console.log(`this.question....`, this.question)
-    this.$store.dispatch('getOneRoom', this.$route.params.id)
-    console.log(this.player2, '================')
+  created() {
+    this.$store.dispatch("getQuestions");
+    console.log(`this.question....`, this.question);
+    this.$store.dispatch("getOneRoom", this.$route.params.id);
+    console.log(this.player2, "================");
   },
   watch: {
-    route (val) {
-      this.$store.dispatch('getOneRoom', this.$route.params.id)
+    route(val) {
+      this.$store.dispatch("getOneRoom", this.$route.params.id);
     }
   },
   computed: mapState([
-    'player1',
-    'player2',
-    'point1',
-    'point2',
-    'roomId',
-    'questions',
-    'ques'
+    "player1",
+    "player2",
+    "point1",
+    "point2",
+    "roomId",
+    "questions",
+    "ques"
   ])
-}
+};
 </script>
-
-<style>
-</style>
